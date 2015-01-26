e5c749502b363b3134045a435a0bfae3ed8aceaa
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-01-26 11:05:48 -0800

    Added Dude.  Dude is alive and moves.

--- DoctorWizard/GameScene.swift
+++ DoctorWizard/GameScene.swift
@@ -9,11 +9,79 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let dude: SKSpriteNode = SKSpriteNode(imageNamed: "dude")
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPointZero
+    var lastTouchLocation: CGPoint?
+
+    
     override func didMoveToView(view: SKView) {
+        
+        dude.position = CGPoint(x: 400, y: 400)
+        addChild(dude)
       
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        println("\(dt*1000) milliseconds since last update")
+        
+        if let lastTouch = lastTouchLocation {
+            let diff = lastTouch - dude.position
+            if (diff.length() <= zombieMovePointsPerSec * CGFloat(dt)) {
+                dude.position = lastTouchLocation!
+                velocity = CGPointZero
+            } else {
+                moveSprite(dude, velocity: velocity)
+                //rotateSprite(dude, direction: velocity, rotateRadiansPerSec: zombieRotateRadiansPerSec)
+            }
+        }
+
+        
+        
+        
+        
     }
+    
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = velocity * CGFloat(dt)
+        println("Amount to move: \(amountToMove)")
+        sprite.position += amountToMove
+    }
+    
+    func moveZombieToward(location: CGPoint) {
+        let offset = location - dude.position
+        let direction = offset.normalized()
+        velocity = direction * zombieMovePointsPerSec
+    }
+    
+    func sceneTouched(touchLocation:CGPoint) {
+        lastTouchLocation = touchLocation
+        moveZombieToward(touchLocation)
+    }
+    
+    override func touchesBegan(touches: NSSet,
+        withEvent event: UIEvent) {
+            let touch = touches.anyObject() as UITouch
+            let touchLocation = touch.locationInNode(self)
+            sceneTouched(touchLocation)
+    }
+    
+    override func touchesMoved(touches: NSSet,
+        withEvent event: UIEvent) {
+            let touch = touches.anyObject() as UITouch
+            let touchLocation = touch.locationInNode(self)
+            sceneTouched(touchLocation)
+    }
+
 }
