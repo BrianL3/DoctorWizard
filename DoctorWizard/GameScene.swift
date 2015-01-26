02c5bd1254015f21944ed3031ce765045057cf07
dhourr <daniel.hour@me.com>
2015-01-26 12:06:46 -0800

    added comment

--- DoctorWizard/GameScene.swift
+++ DoctorWizard/GameScene.swift
@@ -18,6 +18,9 @@ class GameScene: SKScene {
     let playableRect: CGRect
     var lastTouchLocation: CGPoint?
     
+    
+    //MARK: INTIALIZER ===============================================================================
+    
     override init(size: CGSize) {
         let maxAspectRatio:CGFloat = 16.0/9.0 // 1
         let playableHeight = size.width / maxAspectRatio // 2
@@ -31,7 +34,9 @@ class GameScene: SKScene {
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented") // 6
     }
-
+    
+    
+    //MARK: DID MOVE TO VIEW ======================================================================
     
     override func didMoveToView(view: SKView) {
         
@@ -41,19 +46,22 @@ class GameScene: SKScene {
       
     }
     
+    //called before each frame is rendered
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
         
         if lastUpdateTime > 0 {
             dt = currentTime - lastUpdateTime
         } else {
             dt = 0
         }
+        
         lastUpdateTime = currentTime
         println("\(dt*1000) milliseconds since last update")
         
         if let lastTouch = lastTouchLocation {
+            
             let diff = lastTouch - dude.position
+         
             if (diff.length() <= dudeMovePointsPerSec * CGFloat(dt)) {
                 dude.position = lastTouchLocation!
                 velocity = CGPointZero
@@ -63,68 +71,72 @@ class GameScene: SKScene {
         }
         
         boundsCheckDude()
-
-        
     }
     
+    
+    //MARK: MOVE THE DUDE ======================================================================
+
     func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        
         let amountToMove = velocity.y * CGFloat(dt)
+        
         println("Amount to move: \(amountToMove)")
+        
         sprite.position += CGPoint(x: 0, y: amountToMove)
     }
     
     func moveDudeToward(location: CGPoint) {
+        
         let offset = location - dude.position
         let direction = offset.normalized()
+        
         velocity = direction * dudeMovePointsPerSec
     }
     
     func sceneTouched(touchLocation:CGPoint) {
+        
         lastTouchLocation = touchLocation
         moveDudeToward(touchLocation)
     }
     
-    override func touchesBegan(touches: NSSet,
-        withEvent event: UIEvent) {
-            let touch = touches.anyObject() as UITouch
-            let touchLocation = touch.locationInNode(self)
-            sceneTouched(touchLocation)
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
     }
     
-    override func touchesMoved(touches: NSSet,
-        withEvent event: UIEvent) {
-            let touch = touches.anyObject() as UITouch
-            let touchLocation = touch.locationInNode(self)
-            sceneTouched(touchLocation)
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        sceneTouched(touchLocation)
     }
+    
+    
+    //MARK: CHECK BOUNDS ========================================================================
     
     func boundsCheckDude() {
-        let bottomLeft = CGPoint(x: 0,
-            y: CGRectGetMinY(playableRect))
-        let topRight = CGPoint(x: size.width,
-            y: CGRectGetMaxY(playableRect))
         
+        let bottomLeft  = CGPoint(x: 0, y: CGRectGetMinY(playableRect))
+        let topRight    = CGPoint(x: size.width, y: CGRectGetMaxY(playableRect))
         
-        if dude.position.x <= bottomLeft.x {
-            dude.position.x = bottomLeft.x
-            velocity.x = -velocity.x
-        }
-        if dude.position.x >= topRight.x {
-            dude.position.x = topRight.x
-            velocity.x = -velocity.x
-        }
         if dude.position.y <= bottomLeft.y {
             dude.position.y = bottomLeft.y
-            velocity.y = -velocity.y
+            velocity.y = 0
         }
         if dude.position.y >= topRight.y {
             dude.position.y = topRight.y
-            velocity.y = -velocity.y
+            velocity.y = 0
         } 
     }
     
     
-
+//
+//    func checkCollisions() {
+//        
+//        var hitBounds: [] = []
+//    }
 
 
 }
