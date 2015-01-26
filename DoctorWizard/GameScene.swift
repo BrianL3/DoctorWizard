cab9f042de1a53976d5abbd25da4a14443757451
Duncan Marsh <dwolfm@gmail.com>
2015-01-26 10:33:46 -0800

    first init

---
+++ DoctorWizard/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  DoctorWizard
+//
+//  Created by nacnud on 1/26/15.
+//  Copyright (c) 2015 codefellows. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+      
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ DoctorWizard/GameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameViewController.swift
+//  DoctorWizard
+//
+//  Created by nacnud on 1/26/15.
+//  Copyright (c) 2015 codefellows. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .AspectFill
+        skView.presentScene(scene)
+    }
+    override func prefersStatusBarHidden() -> Bool  {
+        return true
+    }
+}
